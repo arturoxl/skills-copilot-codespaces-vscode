81d523954ab7b172829caa607bba8045ba56e52a
arturoql <arturo.04@gmail.com>
2025-02-03 19:45:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,29 @@
+function skillsMember() {
+    // Get the member's skills
+    var memberSkills = {
+        "skills": [
+            {
+                "name": "PHP",
+                "level": "Advanced"
+            },
+            {
+                "name": "JavaScript",
+                "level": "Intermediate"
+            },
+            {
+                "name": "HTML",
+                "level": "Expert"
+            },
+            {
+                "name": "CSS",
+                "level": "Expert"
+            },
+            {
+                "name": "Python",
+                "level": "Intermediate"
+            }
+        ]
+    };
+
+    return memberSkills;
+}
